1ec6a104190de3c64e8f9dd39110dc8ff9fa42b5
RecamFx <camiloreartesgm@gmail.com>
2024-04-10 12:24:43 -0300

    Primera parte

---
+++ explicaciones/1_inputs_print.py
@@ -0,0 +1,12 @@
+# INPUTS: Todo lo que se recibe de input es un string
+
+
+# PRINTS: Que es concatenar? Concatenar es la union de varios elementos (variables).
+# En los prints se pueden usar el + o los fstrings
+
+# Concatenando con +:
+ejemplo = "concatenacion"
+print("Hola " + "esto es una " + ejemplo + " con +") #? Hola esto es una concatenacion con +
+
+# Concatenando con fstrings:
+print(f"Hola esto es una {ejemplo} con fstrings") #? Hola esto es una concatenacion con fstrings
---
+++ explicaciones/2_tipos_de_datos.py
@@ -0,0 +1,33 @@
+# Hay varios tipos de datos
+
+
+# INT: Numeros enteros
+# FLOAT o flotantes: Numeros con coma
+# BOOLS o boleanos: True o False (1 o 0)
+# Strings: Cadenas de texto
+# Arrays: Listas
+
+variable_int = 1
+variable_flotante = 1.2
+variable_booleana = True
+variable_string = "Soy un string"
+variable_arrays = ["Soy", "Una", "Lista"]
+
+
+
+
+# Tambien se pueden crear estas variables con sus respectivas funciones()
+
+variable_int = int("1")
+variable_flotante = float("1.2")
+variable_booleana = bool(0)
+variable_string = str(150)
+variable_arrays = list("Soy una lista")
+
+
+# Aclaraciones
+# int() toma un texto o numero, y lo transforma en un entero (numero si coma)
+# float() toma un texto o numero, y lo transforma un un flotante (numero con coma) 
+# bool() toma 0 y 1, y lo transforma en un True o False
+# str() toma un numero y lo transforma en un texto
+# list() toma un array dentro del parentesis y lo divide por caracteres y lo transforma en una lista
