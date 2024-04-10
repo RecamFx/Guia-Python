7631f28818a965185964459a2ad2d549d312b7c4
RecamFx <camiloreartesgm@gmail.com>
2024-04-09 22:59:40 -0300

    first commit

---
+++ ejercicios/ejercicios.py
@@ -0,0 +1,64 @@
+
+#? PYTHON (17 ejercicios) 
+
+
+#? EJERCICIOS: 1 y 2 
+# Hacer 4 operaciones básicas. 
+
+# Estas en un supermercado. La consola te pide cuanta plata tenes, cuantas unidades de productos vas a llevar y 
+# cuánto cuesta el kilo de producto (cada unidad son 500g, 1kg son dos unidades). 
+# Cuanto va a salir todos los productos y cuanta plata sobra o falta. 
+
+
+#? EJERCICIOS: 1,2 y 5 
+# Hacer un programa que infinitamente te pida: cantidad de productos y valor de productos. 
+# Y que cuando escribas “exit” haga la suma de todos los productos. 
+
+# Vas a unas canchas de futbol. Cada cancha cuesta diferente (5,8,11) y cada horario tiene un precio diferente 
+# (antes de las 12hs: 30% OFF, antes de las 5hs: precio normal y a partir de las 5hs un 25% de aumento). 
+# El usuario ingresa el número de cancha y la hora, devolver el valor. 
+
+#? EJERCICIOS: 1,2,3 y 5 
+# Hacer un programa que te diga si la palabra 1 está dentro de la palabra dos 
+# (ambas ingresadas por consola) [hace lo usando el IN y sin usar el IN]. 
+
+# Hacer una calculadora sencilla, le decís si queres sumar, restar, multiplicar o dividir, 
+# y cuando seleccionas que te pida dos números los cuales vas a realizar la cuenta y la vas a imprimir. 
+
+#? EJERCICIO: 1,2,3,4 y 5 
+# Crear un programa que ya tenga almacenado en variables un nombre, una edad y una contraseña. 
+# En la consola nos va a pedir nombre y contraseña, si son correctos nos va a devolver el año de nacimiento según la edad 
+
+#? EJERCICIO: 1,2,3,4,5 y 6 
+# Hacer una calculadora sencilla donde los cálculos se realicen dentro de funciones 
+# (una para sumar otra para restar y así), le decís si queres sumar, restar, multiplicar o dividir, 
+# y cuando seleccionas que te pida dos números los cuales vas a realizar la cuenta y la vas a imprimir. 
+
+#? EJERCICIOS: 1,2,3,4,5,6 y 7 
+# Crear un programa que nos diga si el numero ingresado es primo 
+
+# Crear un programa que nos diga si el numero ingresado es par o impar 
+
+# Hacer una ruleta, que le pueda ingresar tantas opciones como quiera 
+# y al escribir “GIRAR” elija un resultado de forma aleatoria. 
+
+# Hacer un generador random de contraseñas, 
+# que genere caracteres random (mayúsculas, minúsculas, números y/o puntos y guiones bajos y medios). 
+
+# Hace una lista de supermercado, la consola nos pide el articulo y la cantidad, 
+# y que nos imprima la lista cada vez que la modificamos, nos debe quedar así: 
+# leche (2), queso (1), manzana (5), harina (2). Si repetimos el artículo se le agregará en la cantidad que dijimos. 
+
+# Hacer un login, donde hay 2 opciones, logearse y registrarse. 
+# Donde registrarse te pide el usuario y contraseña, y lo almacena en una variable. 
+# Y cuando nos logueamos ponemos usuario y contraseña, y si es correcto nos sale un mensaje de que nos logueamos! 
+# (¡Pueden haber más de 1 usuario!) 
+
+# Hacer un bloc de notas, nos pide dos opciones, una es "crear nota" y otra es “ver notas”, 
+# si le damos a crear nota vamos a poder escribir una nota. Al seleccionar “ver notas” nos debería imprimir todas las notas 
+# (opcional: que tenga una opción de editar notas). 
+
+# Crear un ahorcado 
+
+# Hacer un programa que verifique los números de un array, 
+# y me diga cual es el mayor y cuál es el menor (sin usar min y max).
---
+++ explicaciones/1_inputs_print.py
@@ -0,0 +1,11 @@
+# INPUTS: Todo lo que se recibe de input es un string
+
+# PRINTS: Que es concatenar? Concatenar es la union de varios elementos (variables).
+# En los prints se pueden usar el + o los fstrings
+
+# Concatenando con +:
+ejemplo = "concatenacion"
+print("Hola " + "esto es una " + ejemplo) #? Hola esto es una concatenacion
+
+# Concatenando con fstrings:
+print(f"Hola esto es una {ejemplo}") #? Hola esto es una concatenacion
