6e3723a4e61bdd7f4c2cdae9f72c905b113ed712
RecamFx <camiloreartesgm@gmail.com>
2024-04-11 21:13:41 -0300

    random, y mas usados

---
+++ mas_usados.py
@@ -0,0 +1,10 @@
+# INDEX: buscar un rango
+
+# JOIN: de lista a string
+# SPLIT: de strings a listas
+
+# COUNT: cuantos valores hay en la lista
+
+# APPEND: agrega un valor a la lista
+# POP: elimina una posicion de la lista
+# REMOVE: elima un valor especifico de la lista
